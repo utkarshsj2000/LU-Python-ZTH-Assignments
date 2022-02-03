d687988e627b90e2e4f3b59c5f2c84e37212fd7d
Utkarsh Shekhar Jaiswal <34391452+utkarshsj2000@users.noreply.github.com>
2022-02-03 23:35:23 +0530

    Day 3 LU ZTH Python Assignment

---
+++ LU_ZTH_Day3.py
@@ -0,0 +1,7 @@
+#Assignment: Take multiple numbers through input and print the sum of the numbers.\
+
+country = {"India": 135, "Pakistan": 30, "USA": 33, "China": 140, "United Kingdom": 6.72}
+print(country)
+
+Country_Population = max(country, key=country.get)
+print('Country with Maximum Population is :', Country_Population)
